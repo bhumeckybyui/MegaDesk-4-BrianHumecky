abb4a51b209a10b058d88839194d42fa9a6edf35
bhumeckybyui <hum13006@byui.edu>
2018-02-06 18:30:57 -0800

    Add project files.

---
+++ MegaDesk/quoteForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk
+{
+    public partial class quoteForm : Form
+    {
+        public quoteForm()
+        {
+            InitializeComponent();
+            this.Text = "View Quote";
+        }
+    }
+}
